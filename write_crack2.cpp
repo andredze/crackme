d56665d88530513bd62e4c226f2b932f0a04a972
anki <an.kiselev.2008@gmail.com>
2026-03-10 01:26:37 +0300

    added vulnerability w stack

---
+++ write_crack2.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARR_SIZE 0xFFFA - 0x0274 + 2
+// FFFA - 0274 == пустые
+int main()
+{
+    FILE* CrackFp = NULL;
+
+    if ((CrackFp = fopen("crack2.bin", "wb")) == NULL)
+    {
+        fprintf(stderr, "Open file error\n");
+        return EXIT_FAILURE;
+    }
+    char crack_input_data[ARR_SIZE] = {0x11};
+
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        crack_input_data[i] = 0x11;
+    }
+
+    crack_input_data[ARR_SIZE - 2] = 0x7B;
+    crack_input_data[ARR_SIZE - 1] = 0x01;
+
+    if (fwrite(crack_input_data, ARR_SIZE, 1, CrackFp) != 1)
+    {
+        fprintf(stderr, "Open file error\n");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stderr, "Write success\n");
+
+    fclose(CrackFp);
+
+    return EXIT_SUCCESS;
+}
